753e575452fff18dbfe21c9a2820e76899effba6
wilsonsihombing <112602489+wilsonsihombing@users.noreply.github.com>
2023-06-03 21:40:15 +0700

    update

--- js/script.js
+++ js/script.js
@@ -4,5 +4,4 @@ const navbarNav = document.querySelector('.navbar-nav');
 // Ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
-}; 
-   
+}
