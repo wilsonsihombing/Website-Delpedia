e5762822212abfde9d1ca0fb760235b3181b61e8
wilsonsihombing <112602489+wilsonsihombing@users.noreply.github.com>
2023-06-04 15:38:33 +0700

    update semua repository

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@ const navbarNav = document.querySelector('.navbar-nav');
 
 // Ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active'); 
+  navbarNav.classList.toggle('active');
 }
+
  
+//Klik di luar sidebar untuk menghilangkan navbar
+const hamburger = document.querySelector ('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    navbarNav.classList.remove('active');
+  }
+
+})
